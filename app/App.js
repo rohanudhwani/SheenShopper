d2e3c281953ecf387b8b6bb2e5989ec9d842058b
Rohan Udhwani <rohanudhwani2@gmail.com>
2023-12-14 00:48:48 +0530

    server setup

---
+++ app/App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-xl text-red-400">App</Text>
+    </View>
+  )
+}
+
+export default App
